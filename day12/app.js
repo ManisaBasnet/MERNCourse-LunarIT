e6ac0f28e299cd63eb94fa1b451d1c3c7ab36727
ManisaBasnet <manishabasnet601@gmail.com>
2024-07-22 14:49:12 +0545

    DAY 12

---
+++ day12/app.js
@@ -0,0 +1,1 @@
+console.log("Hello Manisha bbg")
---
+++ day12/script.js
@@ -0,0 +1,66 @@
+console.log("Bye Manisha bbg")
+
+
+
+var x = "Manisha bbg"
+console.log(x)
+
+var y = 21
+console.log(y)
+
+var z = false
+console.log(z)
+
+var name = "Manisha Don"
+console.log("My name is",name)
+
+
+
+var name1 = "Manisha"
+var name2 = "Manish"
+var name3 = "Anisha"
+var name4 = "Anish"
+var name5 = "Nisha"
+var name = [name1 , name2 , name3, name4, name5]
+console.log(name)
+console.log(name.length)
+console.log(name[1])
+
+
+
+var myself = {
+    "name" : "Manisha Basnet",
+    "age" : 21
+}
+console.log(myself.age)
+
+
+
+var data =[{
+    name : "Manisha",
+    age : 21
+},
+{
+    name : "Anish",
+    age : 23
+}]
+
+var data2 =[{
+    name : "Anish",
+    age : 23
+},
+{
+    name : ["Manisha", "Anish", "Pranjal"],
+    age : 14
+}]
+
+var data3 ={
+    data :[ {
+        name : ["Manisha", "Anish"],
+        age :12
+    }]
+}
+console.log(data[0].name) 
+console.log(data[1].age)
+console.log(data2[1].name[2])
+console.log(data3.data[0].name[1])
